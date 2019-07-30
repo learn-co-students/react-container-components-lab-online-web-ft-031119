e4195aaeee79be89b84fc91830eac0b677e1bc84
brosie224 <barosengarten@gmail.com>
2019-07-30 01:44:22 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
-// Code MovieReviews Here
+import React from "react";
+
+const Review = ({ title, critic }) => (
+  <div className="review" key={title}>
+    <h3>{title}</h3>
+    <p>{critic}</p>
+  </div>
+);
+
+const MovieReviews = ({ reviews }) => {
+  return (
+    <div className="review-list">
+      {reviews.map(review => (
+        <Review title={review.display_title} critic={review.critics_pick} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieReviews;
